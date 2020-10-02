485cf98ab9fe4f7d63803ffcb5cd8ced73719b23
Scott Allen <scott-allen@lambdastudents.com>
2020-10-02 09:41:53 -0600

    Finished with the Tabs.js tasks

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,28 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+const entryTab = document.querySelector('.topics')
+
+function tabMaker(topic){
+    const topicDiv = document.createElement('div')
+
+    topicDiv.classList.add('tab')
+
+    topicDiv.textContent = topic
+
+    return topicDiv
+}
+
+axios.get('https://lambda-times-api.herokuapp.com/topics')
+    .then(res => {
+        const topics = res.data.topics
+        topics.forEach(function(topic){
+            const topicTab = tabMaker(topic)
+            console.log(topicTab)
+            entryTab.append(topicTab)
+        })
+    })
+    .catch(err =>{
+        console.log(err)
+        debugger
+    })
