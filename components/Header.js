5185c40b6a2e88adff4990fbcb4e7d952ffa9b4f
Scott Allen <scott-allen@lambdastudents.com>
2020-10-02 09:18:54 -0600

    Finished with the Header.js tasks

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,32 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+const entryPoint = document.querySelector('.header-container')
+
+
+function Header() {
+    const header = document.createElement('div')
+    const date = document.createElement('span')
+    const title = document.createElement('h1')
+    const temperature = document.createElement('span')
+
+    header.appendChild(date)
+    header.appendChild(title)
+    header.appendChild(temperature)
+
+    header.classList.add('header')
+    date.classList.add('date')
+    temperature.classList.add('temp')
+
+    date.textContent = 'MARCH 28, 2020'
+    title.textContent = 'Lambda Times'
+    temperature.textContent = '98°'
+
+    return header
+}
+
+const lambdaHeader = Header()
+entryPoint.append(lambdaHeader)
+
+
+
